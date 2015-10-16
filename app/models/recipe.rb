05d8ced7338d76720410912f31a8fb94eaa2cc63
Vladimir Hale <vladimir.hale@gmail.com>
2015-10-16 14:18:14 -0500

    Seems fixed kahlua-spiked

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 
 ENV["FOOD2FORK_KEY"] = '2cf6a2d23beae9518d7fa73aad4948cc'
-ENV["FOOD2FORK_SERVER_AND_PORT"] = 'http://food2fork.com'
+ENV["FOOD2FORK_SERVER_AND_PORT"] = nil
 
 class Recipe
 	include HTTParty
@@ -8,7 +8,7 @@ class Recipe
 	key_value = ENV["FOOD2FORK_KEY"]
 	hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || 'www.food2fork.com'
 
-	base_uri "http://food2fork.com/api"
+	base_uri "http://#{hostport}/api"
 	default_params :key => key_value
 	format :json
 
